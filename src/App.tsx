29e9b7560654a08b748ed77ec9210a7488c778be
MediumThor <liltimefrog@protonmail.com>
2025-11-20 15:31:02 -0600

    Remove ChatWidget from app shell

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
-import ChatWidget from './components/ChatWidget';
 import ProtectedRoute from './components/ProtectedRoute';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -82,7 +81,6 @@ function App() {
             </Routes>
           </main>
           <Footer />
-          <ChatWidget />
         </div>
       </Router>
     </AuthProvider>
